e599c022861fc6a8ffcb3306f2661cf8aa517734
cyubro195 <cyubro195@gmail.com>
2024-06-10 17:43:34 +0900

    ㅜㅜ

---
+++ workspace_java/ObjectOriented/src/class_basic/workerstudent/WorkerStudent.java
@@ -0,0 +1,21 @@
+package class_basic.workerstudent;
+
+public class WorkerStudent {
+
+    //회사원 – 이름, 나이, 주소, 회사명, 부서, 급여
+    //학생 – 이름, 나이, 주소, 국어점수, 영어점수, 수학점수, 총점, 평균
+    private String name;
+    private int age;
+    private String addr;
+
+    public PersonInfo(String name, int age, String addr){
+        this.name = name;
+        this.age = age;
+        this.addr = addr;
+    }
+
+
+
+
+
+}
